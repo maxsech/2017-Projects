2731edb554ddb0fb6b33823b9d0f7eec2f978b2f
maxsech <34286036+maxsech@users.noreply.github.com>
2017-12-28 17:38:01 -0600

    Update causeMutationImage.m

--- Generative Algorithm/Image/causeMutationImage.m
+++ Generative Algorithm/Image/causeMutationImage.m
@@ -17,17 +17,17 @@ function [mutatedChild] = causeMutationImage(child, mutationRange, mutationRate)
 % OUTPUTS: output -- The newly mutated child
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 mutatedChild = child ;
-completelyRandomChance = 25;
+completelyRandomChance = 25; %The chance that after a pixel is selected to be mutated that that mutation is completely random
 
 [childrows, childcolumns] = size(child);
 
 for r = 1 : childrows
     for c = 1 : childcolumns
-        rando1 = randi([1, 100]);
+        rando1 = randi([1, 100]); %Generating a value between 1 and 100 to determine if the pixel is altered at all
 
-        if rando1 <= mutationRate
+        if rando1 <= mutationRate %If the first rando is less than or equal to the mutation rate, then the pixel is mutated
             
-            rando2 = randi([1, 100]);
+            rando2 = randi([1, 100]);   %Generating another value between 1 and 100 to determine if the pixel is altered under goes a change within a range or is completely random
 
             if rando2 <= completelyRandomChance
                     dna = randi([1, 255]);
